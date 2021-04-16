68b91a00df4e3ee544da107a9db67f8ec2ec9fc8
brijgogogo <brijgogogo1@gmail.com>
2021-04-16 23:05:24 +0100

    added nvidia settings for brightness control

--- sys_files/create_links.sh
+++ sys_files/create_links.sh
@@ -9,3 +9,4 @@ sys_files_path="/home/vik/sys_files"
 cp $sys_files_path/10-monitor.conf /etc/X11/xorg.conf.d/
 cp $sys_files_path/30-touchpad.conf /etc/X11/xorg.conf.d/
 cp $sys_files_path/00-keyboard.conf /etc/X11/xorg.conf.d/
+cp $sys_files_path/10-nvidia-drm-outputclass.conf /usr/share/X11/xorg.conf.d/
