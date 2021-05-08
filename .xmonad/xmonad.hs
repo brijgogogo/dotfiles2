9d87260bb24f7ecf99a1cbcc23c4fe0df27c432b
Brij Sharma <brijgogogo1@gmail.com>
2021-05-08 18:30:02 +0100

    xmonand changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,24 +1,32 @@
 import XMonad
-import qualified XMonad.StackSet as W
+import Data.Monoid
 import System.IO
+-- import System.Exit
 
 -- Hooks
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
 import XMonad.Prompt.ConfirmPrompt
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
+
+import XMonad.Layout.Gaps
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 
 -- keys
 import Graphics.X11.ExtraTypes.XF86
 
 -- Data
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 -- Utilities
 import XMonad.Util.Dmenu
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+-- import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
 
 myFont :: String
@@ -30,11 +38,19 @@ myModMask = mod4Mask        -- Sets modkey to super/windows key
 myTerminal :: String
 myTerminal = "alacritty"    -- Sets default terminal
 
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+-- Whether clicking on a window to focus also passes the click to the window
+myClickJustFocuses :: Bool
+myClickJustFocuses = False
+
 myBrowser :: String
-myBrowser = "qutebrowser "  -- Sets qutebrowser as browser
+myBrowser = "firefox"  -- Sets qutebrowser as browser
 
 myBorderWidth :: Dimension
-myBorderWidth = 2           -- Sets border width for windows
+myBorderWidth = 0           -- Sets border width for windows
 
 myNormColor :: String
 myNormColor   = "#282c34"   -- Border color of normal windows
@@ -43,21 +59,34 @@ myFocusColor :: String
 myFocusColor  = "#46d9ff"   -- Border color of focused windows
 
 -- Color of current window title in xmobar.
-xmobarTitleColor = "#3d84b8"
+xmobarTitleColor = "#168aad"
 -- Color of current workspace in xmobar.
-xmobarCurrentWorkspaceColor = "#8ab6d6"
+xmobarCurrentWorkspaceColor = "#2a9d8f" -- "#8ab6d6"
 
 windowCount :: X (Maybe String)
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
--- myWorkspaces = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
-myWorkspaces = [" 1:www ", " 2:ter ", " 3:sys ", " 4:doc ", " 5:code ", " 6:chat ", " 7:mus ", " 8:book ", " 9:cal "]
+-- virtual screens
+myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
 
 -- layout hook
 -- myLayoutHook = avoidStruts $ layoutHook defaultConfig
 myLayoutHook = avoidStruts $ layoutHook def
+
+-- Window rules
+-- get property name associated with a program, run below then click window
+-- xprop | grep WM_CLASS
+myManageHook = composeAll
+    [ className =? "MPlayer"        --> doFloat
+    , className =? "mpv"           --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    , resource  =? "kdesktop"       --> doIgnore
+    , stringProperty "_NET_WM_NAME" =? "Emulator" --> doFloat
+    ]
+    -- , isFullscreen                  --> doFullFloat]
 
 myKeys :: [(String, X ())]
 myKeys =
@@ -73,6 +102,8 @@ myKeys =
 
   , ("M-S-p", spawn "screenshot")
 
+  , ("M-,", spawn "firefox")
+
   -- ("M-<Return>", spawn (myTerminal))
   -- , ("M-S-q", confirmPrompt defaultXPConfig "exit" $ io exitSuccess)
   -- , ((0, xF86XK_PowerDown),         spawn "sudo pm-suspend")
@@ -82,20 +113,26 @@ main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc"
   --xmonad $ ewmh def
-  xmonad $ docks def
+  -- xmonad $ docks def
+  xmonad $ docks $ ewmh def
     { terminal    = myTerminal
     , modMask     = myModMask
     , workspaces = myWorkspaces
     , borderWidth = myBorderWidth
     , normalBorderColor = myNormColor
     , focusedBorderColor = myFocusColor
+
+    , handleEventHook = handleEventHook def <+> fullscreenEventHook
     , layoutHook = myLayoutHook
     , manageHook = manageDocks <+> manageHook def
     , logHook = dynamicLogWithPP $ xmobarPP {
             ppOutput = hPutStrLn xmproc
           , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
-          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-          , ppSep = "   "
+          , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor "" . wrap "" ""
+          , ppUrgent  = xmobarColor "#ff69b4" ""
+          -- , ppSep = "   "
+          , ppSep = xmobarColor "#545454" "" " | " -- before title
+          , ppLayout = const "" -- to disable the layout info on xmobar
       }
     } `additionalKeysP` myKeys
 
