c8a6ea07cf504623d8922325eb53eefe7fecdb08
Brij Sharma <brijgogogo1@gmail.com>
2024-01-07 01:09:00 +0000

    migrated to toml config for alacritty, cleaned up reshift config

--- bin/connect-monitors.sh
+++ bin/connect-monitors.sh
@@ -9,9 +9,9 @@ ext2_on=$(xrandr | grep "$extern2 connected")
 if [ ! -z "$ext1_on" ] && [ ! -z "$ext2_on" ]
 then
   # xrandr --output "$extern2" --mode 2560x1440 --pos 0x0 --rotate normal --output "$extern1" --mode 1920x1080 --pos 2560x-310 --rotate right --output "$intern" --off
-  xrandr --output "$extern2" --mode 2560x1440 --pos 0x0 --rotate normal --output "$extern1" --mode 1920x1080 --pos 2560x0 --rotate normal --output "$intern" --off
+  # xrandr --output "$extern2" --mode 2560x1440 --pos 0x0 --rotate normal --output "$extern1" --mode 1920x1080 --pos 2560x0 --rotate normal --output "$intern" --off
   # xrandr --output "$extern1" --rotate right
-  # xrandr --output "$extern1" --mode 1920x1080 --pos 0x0 --rotate normal --output "$extern2" --mode 2560x1440 --pos 1920x0 --rotate normal --output "$intern" --off
+  xrandr --output "$extern1" --mode 1920x1080 --pos 0x0 --rotate left --output "$extern2" --mode 2560x1440 --pos 1080x300 --rotate normal --output "$intern" --off
 elif [ ! -z "$ext2_on" ]
 then
 	xrandr --output "$extern2" --auto --output "$intern" --off
